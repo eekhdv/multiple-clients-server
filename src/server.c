95ff00db6fee87563a32323a2ec2540fe3c96d94
khadiev <khadiev.edem@gmail.com>
2022-06-04 13:49:37 +0300

    * Server for one client

---
+++ src/server.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define PORT "8765"
+
+
+int main(void) {
+	int status, sockfd, new_fd, yes = 1; 
+	struct addrinfo *servinfo;
+	struct addrinfo hints = {
+		.ai_family = AF_UNSPEC,   // no metter iPv6 of iPv4. use AF_INET and AF_INET6 for iPv4 and iPv6 respectively
+		.ai_socktype = SOCK_STREAM, // make TCP type
+		.ai_flags = AI_PASSIVE, 
+	};
+	if ((status = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo error: %s", gai_strerror(status));
+		exit(1);
+	}
+	if ((sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol)) == -1) {
+		perror("socket"); // _fd for file descriptor
+	}
+	/* fun fact:
+	 * PF_INET == AF_INET, but P is protocol and A is address. 
+	 * Difference is that Address Family (AF) might support several protocols were reffered by their Protocol Family (PF)
+	 * */
+	
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &yes, sizeof yes)) {
+		perror("setsockopt");
+	}
+	if (bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) < 0) {
+		perror("bind failed");
+	}
+	freeaddrinfo(servinfo);
+	if (listen(sockfd, 10) < 0) {
+		perror("listen");
+	}
+	struct sockaddr_storage their_addr;
+	socklen_t addr_size = sizeof their_addr;
+	char recv_buf[65536];
+
+	while (1) {
+		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size)) == -1) {
+			perror("accept");
+		}
+		recv(new_fd, recv_buf, 65536, 0);
+		printf("%s", recv_buf);
+		close(new_fd);
+		sleep(1);
+	}
+	return 0;
+}
