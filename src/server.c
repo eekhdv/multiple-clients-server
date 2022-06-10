18a8d0bb75f84920ab9b75e6d88ac57b78e1bcbd
khadiev <khadiev.edem@gmail.com>
2022-06-10 09:27:37 +0300

    add USER_INFO structure

--- src/server.c
+++ src/server.c
@@ -28,18 +28,25 @@
 
 #define PORT "8765"
 
+
 FILE *database;
 pthread_t thread_clients[100];
+typedef struct {
+	char username[20];
+	int room_number;
+	int sockfd;
+} USER_INFO;
+
 
 void *client_connection(void *vargp) {
-	int clientfd = *(int *)vargp;
+	USER_INFO *user = vargp;
 	char buffer[65536] = {'\0'};
 	while (1) {
 		memset(buffer, 0, sizeof buffer);
-		recv(clientfd, buffer, 65536, 0);
+		recv(user->sockfd, buffer, 65536, 0);
 		if (strcmp(buffer, "exit\n") == 0) {
-			printf("Client %d disconnected\n", clientfd);
-			close(clientfd);
+			printf("[Disconnected] Client(%d) %s \n", user->sockfd, user->username);
+			close(user->sockfd);
 			break;
 		} 
 		printf("%s", buffer);
@@ -90,10 +97,19 @@ int init_server() {
 	return sockfd;
 }
 
+void init_user(USER_INFO user, int sockfd, int room_number, char *username) {
+	// user.sockfd = sockfd;
+	// user.room_number = room_number;
+	// strcpy(user.username, username);
+	// user.username[strlen(username) - 1] = '\0';
+	return;
+}
+
 void client_access(int sockfd) {
 	struct sockaddr_storage their_addr;
 	socklen_t addr_size = sizeof their_addr;
 	char recv_buf[65536];
+	USER_INFO user[100];
 
 	int new_fd;
 	int i = 0;
@@ -103,10 +119,13 @@ void client_access(int sockfd) {
 		}
 		recv(new_fd, recv_buf, sizeof recv_buf, 0);
 		if (is_room_correct(recv_buf)) {
-			printf("[Connected] Client %d\n", new_fd);
-			pthread_create(&thread_clients[i++], NULL, client_connection, (void *)&new_fd);
+			int room_number = atoi(recv_buf);
+			memset(recv_buf, 0, sizeof recv_buf);
+			recv(new_fd, recv_buf, sizeof recv_buf, 0);
+			init_user(user[i], new_fd, room_number, recv_buf);
+			pthread_create(&thread_clients[i++], NULL, client_connection, user);
 		} else {
-			char *error_message = "[Error] Room number is incorrect";
+			char *error_message = "[Error] Room number is incorrect\n";
 			send(new_fd, error_message, strlen(error_message), 0);
 			close(new_fd);
 		}
@@ -115,11 +134,9 @@ void client_access(int sockfd) {
 }
 
 int main(void) {
-	database = fopen("./userdata.txt", "a");
 	int sockfd = init_server();
 
 	client_access(sockfd);
 
-	fclose(database);
 	return 0;
 }
